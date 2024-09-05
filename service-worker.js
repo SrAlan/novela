d477e556ba3817e17c3f7aa8eb7877a2ef1d3339
Alan Ledesma <aledesma@posibillian.tech>
2024-09-05 15:26:01 -0400

    new game

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'rh-simulator';
+var cacheName = 'cam001';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
