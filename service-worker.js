bbee2c34b8a489318d131ab54bb34acfb0a7748a
Alan Ledesma <aledesma@posibillian.tech>
2024-10-01 11:48:00 -0400

    only

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'rh-simulator';
+var cacheName = 'yet-another-phone';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
